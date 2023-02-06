7b8458a89c60902204cdd4ed18e633f7943cc47e
daidalos99 <daidalos0430@gmail.com>
2023-02-06 11:49:54 +0900

    20230205

--- 14425.cpp
+++ 14425.cpp
@@ -8,10 +8,12 @@ using namespace std;
 
 // map에 입력받은 문자열과 true를 입력해주고, 입력받은 문자열이 map에 존재하면
 // 문자열의 개수를 +1
+// map: 왼쪽이 key값, 오른쪽이 value값
+// key와 value를 가지는 노드를 생성하여 정렬된 트리형태로 저장해둠
 map<string, bool> m;
 int N, M;
 string str;
-int answer = 0;
+int num = 0;
 
 int main(void){
     cin >> N >> M;
@@ -21,7 +23,7 @@ int main(void){
     }
     for(int i = 0; i < M; i++){
         cin >> str;
-        if(m[str]==true) answer++;
+        if(m[str]==true) num++;
     }
-    cout << answer << endl;
+    cout << num << endl;
 }
